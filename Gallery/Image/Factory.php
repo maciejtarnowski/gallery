567bef68c4ccc00d64de28cfd27a6d4dee8027ff
Maciej Tarnowski <maciej@tarnowski.it>
2016-01-10 21:48:35 +0100

    Validate image data in factory

--- Gallery/Image/Factory.php
+++ Gallery/Image/Factory.php
@@ -6,15 +6,32 @@ use Gallery\Image;
 
 class Factory
 {
-    public function getImage(array $imageData)
+    public function getImage($imageData)
     {
+        if (!$this->isImageDataValid($imageData)) {
+            return null;
+        }
+
         return new Image($imageData['name'], $imageData['filename'], new Dimensions($imageData['height'], $imageData['width']));
     }
 
     public function getImages(array $imagesData)
     {
         foreach ($imagesData as $imageData) {
+            if (!$this->isImageDataValid($imageData)) {
+                continue;
+            }
+
             yield $this->getImage($imageData);
         }
+    }
+
+    private function isImageDataValid($imageData)
+    {
+        return is_array($imageData) && empty(
+            array_unique(array_keys($imageData), [
+                'name', 'filename', 'height', 'width'
+            ])
+        );
     }
 }
